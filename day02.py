b0a077c76eb21240251f9cf8c5426953e04f4857
PhaseRush <PhaseAnsible@gmail.com>
2021-12-02 00:59:22 -0800

    Use case matching for d2

--- day02.py
+++ day02.py
@@ -3,6 +3,7 @@ import logging
 from aocd import submit
 from aocd.models import Puzzle
 
+logging.basicConfig(level=logging.INFO)
 puzzle = Puzzle(year=2021, day=2)
 input = [line.split() for line in puzzle.input_data.splitlines()]
 input = [(tuple[0], int(tuple[1])) for tuple in input]
@@ -13,12 +14,14 @@ def part1():
     horizontal = 0
 
     for command in input:
-        if command[0] == "forward":
-            horizontal += command[1]
-        elif command[0] == "down":
-            depth += command[1]
-        else:
-            depth -= command[1]
+        match command:
+            case ["forward", int(o)]:
+                horizontal += o
+            case ["down", int(o)]:
+                depth += o
+            case ["up", int(o)]:
+                depth -= o
+
 
     logging.info(depth * horizontal)
     submit(depth * horizontal, part='a', day=2, year=2021)
@@ -30,13 +33,14 @@ def part2():
     aim = 0
 
     for command in input:
-        if command[0] == "forward":
-            horizontal += command[1]
-            depth += aim * command[1]
-        elif command[0] == "down":
-            aim += command[1]
-        else:
-            aim -= command[1]
+        match command:
+            case ["forward", int(o)]:
+                horizontal += o
+                depth += aim * o
+            case ["down", int(o)]:
+                aim += o
+            case ["up", int(o)]:
+                aim -= o
 
     logging.info(depth * horizontal)
     submit(depth * horizontal, part='b', day=2, year=2021)
